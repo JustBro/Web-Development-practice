d04b029ef4179be86b3401d623d991fa98d0986a
Vlad Oboturov <vlad100493@gmail.com>
2023-03-03 17:28:09 +0300

    Пробелы в JS поправил
    Пробелы в JS поправил

--- script.js
+++ script.js
@@ -12,20 +12,22 @@ onresize = () => {
   addSwiper();
 };
 
-if (screenWidth < 768) {
-  addSwiper();
-};
+if (screenWidth < 768) {addSwiper();};
 
 function deploybrands() {
+
   if (!brandsExpDeploy) {
+
     brandsList.classList.add('brands__list--expund');
     brandsExp.innerHTML =`
-      <div class="expunder__pic" 
-           style="transform: rotate(180deg);">
+      <div  class="expunder__pic" 
+            style="transform: rotate(180deg);">
       </div>
       Скрыть`;
     brandsExpDeploy = true;
+
   } else if (brandsExpDeploy) {
+
     brandsList.classList.remove('brands__list--expund');
     brandsExp.innerHTML = `
       <div class="expunder__pic"></div>
@@ -35,7 +37,9 @@ function deploybrands() {
 };
 
 function addSwiper () {
+
   if (screenWidth < 768 && swiper == undefined) {
+
     brands.classList.add('swiper');
     brandsList.classList.add('swiper-wrapper');
     brandsElems.forEach((elem) => {
@@ -52,13 +56,16 @@ function addSwiper () {
       },
     
     });
+
   } else if (screenWidth >= 768 && swiper != undefined) {
+
     location.reload();
     brands.classList.className = ('brands');
     brandsList.classList.className = ('brands__list');
     brandsElems.forEach((elem) => {
       elem.parentElement.className = ''
     });
+
     swiper = undefined;
     delete swiper;
     swiper.destroy();
